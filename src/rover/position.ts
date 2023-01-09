2528d884f805bd5c42b5d88e9eb00dfb089ad35f
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-09 17:43:01 +0000

    processing input coords wip

--- src/rover/position.ts
+++ src/rover/position.ts
@@ -3,6 +3,14 @@ interface Coord {
     y: number
 };
 
+export function isValidCoord(inputCoord : string) : Boolean {
+    // true if fits pattern of number space number
+    // split by space should give an array of length 2
+    // each string should be only digits
+    const coordArray = inputCoord.split(" ");
+    return coordArray.length === 2 && coordArray.every(item => /d+/g.test(item));
+}
+
 function inputToCoord(inputCoord : string) : Coord {
     return {
         x: parseInt(inputCoord[0]),
@@ -11,6 +19,9 @@ function inputToCoord(inputCoord : string) : Coord {
 };
 
 export function setPlateau(inputCoord: string) : void {
-    const plateauCoord = inputToCoord(inputCoord);
-    console.log(plateauCoord);
+    if (isValidCoord(inputCoord)) {
+        const plateauCoord = inputToCoord(inputCoord);
+        console.log(plateauCoord);
+    }
+    else throw new Error("Not a valid coordinate. Must be number space number");
 }
--- src/tests/position.test.ts
+++ src/tests/position.test.ts
@@ -1,1 +1,7 @@
+import { isValidCoord } from '../rover/position';
 
+describe("test isValidCoord", () => {
+  it("should return true for 5 5", () => {
+    expect(isValidCoord("5 5")).toBe(true);
+  })
+})
