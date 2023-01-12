4369ce1e588a00d8c5a70d081ade9cea9663e405
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-12 14:27:04 +0000

    refactored instruction loop in index and removed comments

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,23 @@
 import { clear, print, input, end } from './console';
 import { createRover, moveRover } from './controller';
 
-// function that starts the mars rover
 export async function startRover() {
     clear();
     print("Mars Rover initiating.......");
     const inputPlateau = await input("Enter the maximum Plateau Coord:");
     const inputPosition = await input("Enter the rover initial position:");
-    // 1. create a rover
     const rover = createRover(inputPlateau, inputPosition);
-    // 2. ask for instructions to move
-    async function userMoveRover() {
-        const inputInstructions = await input("Enter instructions to move the rover:");
-        const currentPosition = moveRover(rover, inputInstructions);
-        // 3. return end position
-        console.log(`Rover's current position: ${currentPosition.x} ${currentPosition.y} ${currentPosition.direction}`);
-        const additionalMoves = await input("Would you like to move the Rover again?");
-        return additionalMoves;
-    }
-    let inputAdditionalMoves = await userMoveRover();
-    // 4. want to move again? Y - step 2. N - step 5
+    let inputAdditionalMoves = "Y";
     if (inputAdditionalMoves === "Y") {
         do {
-            inputAdditionalMoves = await userMoveRover();
+            const inputInstructions = await input("Enter instructions to move the rover:");
+            const currentPosition = moveRover(rover, inputInstructions);
+            console.log(`Rover's current position: ${currentPosition.x} ${currentPosition.y} ${currentPosition.direction}`);
+            inputAdditionalMoves = await input("Would you like to move the Rover again?");
         } while (inputAdditionalMoves === "Y");
     }
-    // 5. End readline with message
     console.log("Rover out");
     end();
 }
 
-// call function
 startRover();
