2a21ef20de7564b1905741e61b6a9debfbb7fd24
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-12 09:33:49 +0000

    Async inputs working using promises to avoid nested callbacks in console logic

--- src/console.ts
+++ src/console.ts
@@ -17,20 +17,26 @@ const reader = readline.createInterface({
 	output: process.stdout,
 });
 
-export function getInputs(plateauQ : string, initialPositionQ : string, instructionsQ : string) {
-	reader.question(plateauQ, (plateauInput) => {
-		const plateauCoord = setPlateau(plateauInput);
-		reader.question(initialPositionQ, (positionInput) => {
-			const initialPosition = setInitialPosition(positionInput);
-			const rover = new Rover(plateauCoord, initialPosition);
-			console.log(rover);
-			reader.question(instructionsQ, (instructionsInput) => {
-				const instructionArray = setInstructionSet(instructionsInput);
-				console.log("End position for Rover:");
-				// TODO: convert Postion type back to string for diaply
-				rover.move(instructionArray);
-				console.log(rover.position);
-			})
-		})
+export async function input(textQ : string) : Promise<string> {
+	return new Promise ((resolve) => {
+		reader.question(textQ, (answer) => resolve(answer));
 	});
 }
+
+// export function getInputs(plateauQ : string, initialPositionQ : string, instructionsQ : string) {
+// 	reader.question(plateauQ, (plateauInput) => {
+// 		const plateauCoord = setPlateau(plateauInput);
+// 		reader.question(initialPositionQ, (positionInput) => {
+// 			const initialPosition = setInitialPosition(positionInput);
+// 			const rover = new Rover(plateauCoord, initialPosition);
+// 			console.log(rover);
+// 			reader.question(instructionsQ, (instructionsInput) => {
+// 				const instructionArray = setInstructionSet(instructionsInput);
+// 				console.log("End position for Rover:");
+// 				// TODO: convert Postion type back to string for diaply
+// 				rover.move(instructionArray);
+// 				console.log(rover.position);
+// 			})
+// 		})
+// 	});
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
-import { clear, print, getInputs } from './console.js';
+import { clear, print, input } from './console.js';
+
 
 // function that starts the mars rover
-export function startRover() {
+export async function startRover() {
     clear();
     print("Mars Rover initiating.......");
-    getInputs("Enter the maximum Plateau Co-ordinate:", "Enter the rover initial position:", "Enter the set of instructions for the rover:");
+    const inputPlateau = await input("Enter the maximum Plateau Coord:");
+    const inputPosition = await input("Enter the rover initial position:");
+    // 1. create a rover
+    // 2. ask for instructions to move
+    // 3. return end position
+    // 4. want to move again? Y - step 2. N - step 5
+    // 5. End readline with message
+    console.log(`the input value is: ${inputPlateau}`);
+    // getInputs("Enter the maximum Plateau Co-ordinate:", "Enter the rover initial position:", "Enter the set of instructions for the rover:");
 }
 
 // call function
