efec4035a4c66164fcdc2bfd13490d7a6c0b57a0
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-14 11:54:00 +0000

    refactored index / created console "components"

--- src/console.ts
+++ src/console.ts
@@ -1,4 +1,7 @@
 import * as readline from 'node:readline';
+import { createRover, moveRover, isValidCoord, isValidPosition, isValidInstructions, setPlateau } from './controller';
+import { Plateau } from './plateau';
+import { Rover } from './rover';
 
 export function print(str: string): void {
 	console.log(str);
@@ -33,4 +36,61 @@ export async function inputWithValidation(questionText: string, inputIsValid: (a
 export function end(): void {
 	reader.close();
 	console.log("Goodbye!");
+}
+
+export async function consolePlateauSetup() {
+	const inputPlateau = await inputWithValidation(
+		"Enter the maximum Plateau Coord (number number):",
+		isValidCoord,
+		"Incorrect formatting. Please enter coordinate again"
+	);
+	const plateau = setPlateau(inputPlateau);
+	return plateau;
+}
+
+const isAPositiveInteger = (input: string) => {
+	const int = parseInt(input);
+	return isNaN(int) && int > 0;
+}
+
+export async function consoleRoverNumberSetup() {
+	const numberOfRovers = await inputWithValidation(
+		"How many rovers would you like to land?:",
+		isAPositiveInteger,
+		"input is not a number. Please enter a positive integer"
+	);
+	return numberOfRovers;
+}
+
+export async function consoleRoverSetup(numberOfRovers: string, plateau: Plateau) {
+	const rovers = []
+	for (let i = 1; i <= parseInt(numberOfRovers); i++) {
+		const inputPosition = await inputWithValidation(
+			`Enter the rover ${i} initial position (number number direction):`,
+			isValidPosition,
+			"Your input is not of the format x y direction"
+		);
+		const rover = createRover(i, plateau, inputPosition);
+		print("Rover created");
+		rovers.push(rover);
+	}
+	return rovers;
+}
+
+export async function consoleInstructionInputs(rovers: Array<Rover>) {
+	let inputAdditionalMoves = "Y";
+	if (inputAdditionalMoves === "Y") {
+		do {
+			for (let i = 0; i < rovers.length; i++) {
+				const inputInstructions = await inputWithValidation(
+					`Enter instructions to move Rover ${rovers[i].id} (L,R or M):`,
+					isValidInstructions,
+					"Invalid set of instructions. Must only contain L, R or M"
+				);
+				const currentPosition = moveRover(rovers[i], inputInstructions);
+				console.log(`Rover ${rovers[i].id} current position: ${currentPosition.x} ${currentPosition.y} ${currentPosition.direction}`);
+			}
+			inputAdditionalMoves = await input("Would you like to move the Rovers again? Y/N");
+		} while (inputAdditionalMoves === "Y");
+	}
 }
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,13 @@
-import { clear, print, input, inputWithValidation, end } from './console';
-import { createRover, moveRover, isValidCoord, isValidPosition, isValidInstructions, setPlateau } from './controller';
-import { Rover } from './rover';
+import { clear, print, end, consolePlateauSetup, consoleRoverNumberSetup, consoleRoverSetup, consoleInstructionInputs } from './console';
 
 export async function startRover() {
     clear();
     print("Mars Rover Program initiating.......");
-    const inputPlateau = await inputWithValidation(
-        "Enter the maximum Plateau Coord (number number):",
-        isValidCoord,
-        "Incorrect formatting. Please enter coordinate again"
-    );
-    const plateau = setPlateau(inputPlateau);
+    const plateau = await consolePlateauSetup()
     print("Plateau created");
-    //TODO add validation?
-    const numberOfRovers = await input(
-        "How many rovers would you like to land?:"
-    );
-    const rovers = []
-    for (let i = 1; i <= parseInt(numberOfRovers); i++) {
-        const inputPosition = await inputWithValidation(
-            `Enter the rover ${i} initial position (number number direction):`,
-            isValidPosition,
-            "Your input is not of the format x y direction"
-        );
-        const rover = createRover(i, plateau, inputPosition);
-        print("Rover created");
-        rovers.push(rover);
-    }
-    console.log(plateau.roverPositions);
-    let inputAdditionalMoves = "Y";
-    if (inputAdditionalMoves === "Y") {
-        do {
-            for (let i = 0; i < rovers.length; i++) {
-                const inputInstructions = await inputWithValidation(
-                    `Enter instructions to move Rover ${rovers[i].id} (L,R or M):`,
-                    isValidInstructions,
-                    "Invalid set of instructions. Must only contain L, R or M"
-                );
-                const currentPosition = moveRover(rovers[i], inputInstructions);
-                console.log(`Rover ${rovers[i].id} current position: ${currentPosition.x} ${currentPosition.y} ${currentPosition.direction}`);
-            }
-            inputAdditionalMoves = await input("Would you like to move the Rovers again? Y/N");
-        } while (inputAdditionalMoves === "Y");
-    }
+    const numberOfRovers = await consoleRoverNumberSetup();
+    const rovers = await consoleRoverSetup(numberOfRovers, plateau);
+    await consoleInstructionInputs(rovers);
     console.log("Mission control out");
     end();
 }
