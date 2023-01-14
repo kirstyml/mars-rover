e742e7901e527f1f1eb43f9d9a76db33b43f95bf
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-01-14 12:42:53 +0000

    Update rover tests to omit itself from calculating it's own position

--- src/rover.ts
+++ src/rover.ts
@@ -7,7 +7,7 @@ export class Rover {
     position: Position;
 
     constructor(id: number = 1, plateau: Plateau, position: Position) {
-        if (!isValidMove(position, plateau)) {
+        if (!isValidMove(position, plateau, id)) {
             throw new Error("Rover's initial position is invalid. Cannot land outside of the plateau or on another rover")
         }
         this.id = id;
@@ -17,31 +17,31 @@ export class Rover {
     }
 
     move(instructions: Array<string>) {
-        this.position = takeInstructions(this.position, this.plateau, instructions);
+        this.position = takeInstructions(this.position, this.plateau, instructions, this.id);
         this.plateau.updatePositions({ id: this.id, position: this.position });
     }
 };
 
 //TODO: can get Array<InstructionType> working
-export function takeInstructions(initialPosition: Position, plateau: Plateau, instructionSet: Array<string>) {
+export function takeInstructions(initialPosition: Position, plateau: Plateau, instructionSet: Array<string>, id: number) {
     let nextPosition = { ...initialPosition }
     for (let i = 0; i <= instructionSet.length - 1; i++) {
         const inst = instructionSet[i];
         if (inst === "L" || inst === "R" || inst === "M") {
-            nextPosition = makeMove(nextPosition, plateau, inst);
+            nextPosition = makeMove(nextPosition, plateau, inst, id);
         }
         else console.log("invalid move.");
     }
     return nextPosition;
 }
 
-export function isValidMove(position: Position, plateau: Plateau) {
+export function isValidMove(position: Position, plateau: Plateau, id: number) {
     const xPositionValid = position.x <= plateau.maxX && position.x >= plateau.minX;
     const yPositionValid = position.y <= plateau.maxY && position.y >= plateau.minY;
-    const roverPositions = plateau.getRoverPositions();
+    const roverPositions = plateau.roverPositions;
     let roverInWay = false;
     roverPositions.forEach(otherRoverPosition => {
-        if (position.x === otherRoverPosition.x && position.y === otherRoverPosition.y) {
+        if (id !== otherRoverPosition.id && position.x === otherRoverPosition.position.x && position.y === otherRoverPosition.position.y) {
             roverInWay = true;
         }
     });
@@ -49,7 +49,7 @@ export function isValidMove(position: Position, plateau: Plateau) {
 }
 
 //TODO: can get Array<InstructionType> working
-export function makeMove(initialPosition: Position, plateau: Plateau, instruction: InstructionType) {
+export function makeMove(initialPosition: Position, plateau: Plateau, instruction: InstructionType, id: number) {
     const endPosition = { ...initialPosition };
     const indexCurrentDirection = directions.indexOf(initialPosition.direction);
     // spin left if L
@@ -75,7 +75,7 @@ export function makeMove(initialPosition: Position, plateau: Plateau, instructio
             endPosition.x -= 1;
         }
         // validate if can make move
-        if (!isValidMove(endPosition, plateau)) {
+        if (!isValidMove(endPosition, plateau, id)) {
             return initialPosition;
         }
     }
